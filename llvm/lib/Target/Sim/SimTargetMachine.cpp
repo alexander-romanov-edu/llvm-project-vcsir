bef1e1adf08459b9060f0e89b67f08bf0f06fc4c
Lisitsyn Sergey <fox9508@ya.ru>
2025-03-18 19:59:08 -0400

    [Sim] 6. Add empty SimPassConfig

--- llvm/lib/Target/Sim/SimTargetMachine.cpp
+++ llvm/lib/Target/Sim/SimTargetMachine.cpp
@@ -1,6 +1,7 @@
 #include "SimTargetMachine.h"
 #include "Sim.h"
 #include "TargetInfo/SimTargetInfo.h"
+#include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/MC/TargetRegistry.h"
 #include <optional>
 
@@ -23,4 +24,25 @@ SimTargetMachine::SimTargetMachine(const Target &T, const Triple &TT,
           Reloc::Static, getEffectiveCodeModel(CM, CodeModel::Small), OL) {
   SIM_DUMP_CYAN
   initAsmInfo();
+}
+
+namespace {
+
+/// Sim Code Generator Pass Configuration Options.
+class SimPassConfig : public TargetPassConfig {
+public:
+  SimPassConfig(SimTargetMachine &TM, PassManagerBase &PM)
+      : TargetPassConfig(TM, PM) {}
+
+  bool addInstSelector() override {
+    SIM_DUMP_CYAN
+    return false;
+  }
+};
+
+} // end anonymous namespace
+
+TargetPassConfig *SimTargetMachine::createPassConfig(PassManagerBase &PM) {
+  SIM_DUMP_CYAN
+  return new SimPassConfig(*this, PM);
 }
